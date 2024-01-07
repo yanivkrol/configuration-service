75f51bb5cff7e9e92b9186cf979191093a1333e7
yanivkrol <yaniv.krol@naturalint.com>
2024-01-07 14:52:27 +0200

    complete db credentials and url

--- common/db_config.py
+++ common/db_config.py
@@ -9,13 +9,39 @@ from common.parameter_store_api import ParameterStoreApi
 
 
 def _get_engine():
-    env = os.environ.get('ENV', 'dev')
-    db_url = os.environ.get('DB_URL', "aurora-staging.va.naturalint.com:3306")
+    env = _ensure_fully_quantified_env(os.environ.get('ENV'))
+    db_authority = os.environ.get('DB_AUTHORITY')
+    if env == 'development':
+        user, password = _get_credentials_from_env()
+    else:
+        user, password = _get_credentials_from_secrets(env)
+    full_url = f"mysql://{user}:{password}@{db_authority}/configuration_service"
+    return create_engine(full_url)
+
+
+def _ensure_fully_quantified_env(env: str) -> str:
+    if env in ['development', 'staging', 'production']:
+        return env
+    if env == 'dev':
+        return 'development'
+    if env == 'stg':
+        return 'staging'
+    if env == 'prod':
+        return 'production'
+    raise Exception(f'Invalid env: {env}')
+
+
+def _get_credentials_from_env() -> tuple[str, str]:
+    user = os.environ.get('DB_USER')
+    password = os.environ.get('DB_PASSWORD')
+    return user, password
+
+
+def _get_credentials_from_secrets(env: str) -> tuple[str, str]:
     secrets = ParameterStoreApi()
     user = secrets.get_param_value(f'cred_configo_aurora-username_{env}')
     password = urllib.parse.quote(secrets.get_param_value(f'cred_configo_aurora-password_{env}'))
-    full_url = f"mysql://{user}:{password}@{db_url}/configo"
-    return create_engine(full_url)
+    return user, password
 
 
 engine = _get_engine()
